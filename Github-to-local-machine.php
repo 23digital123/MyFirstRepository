c29cdcc9b85d1ec78f7d5cc2b31a3a3d5adf2024
bfx23digital <test.23digital@gmail.com>
2018-11-14 15:43:47 +0530

    edit in local new file create

---
+++ Github-to-local-machine.php
@@ -0,0 +1,10 @@
+<?php echo "welcome to github local server
+send localserver to github
+welcome to github local server
+send localserver to github
+welcome to github local server
+send localserver to github
+welcome to github local server ";
+
+echo phpinfo();
+?>
